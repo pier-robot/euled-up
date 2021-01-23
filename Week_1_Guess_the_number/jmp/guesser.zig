63184587ba5e33b16bedfc66fd772ba3937a0cbd
jim price <shadeops@gmail.com>
2021-01-23 15:09:26 -0800

    Reorganize and add some comments

--- Week_1_Guess_the_number/jmp/guesser.zig
+++ Week_1_Guess_the_number/jmp/guesser.zig
@@ -1,8 +1,11 @@
+// Import the standard library
 const std = @import("std");
 
+// Define the range as constants
 const lowest : i32 = 1;
 const highest : i32 = 100;
 
+// Let's return a type, error and a conditional! :D
 fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
     var line = (try reader.readUntilDelimiterOrEof(
         buffer,
@@ -11,57 +14,76 @@ fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
     return line;
 }
 
-fn gameLoop(reader: anytype, 
-            writer: anytype, 
-            picked_num: i32) !bool {
+// public function main, returns nothing
+pub fn main() void {
 
+    // Here we get stdin and stdout from the std library
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+    
+    // Get a seed value, here we can explicitly state that a var is undefined
+    var seed: u64 = undefined;
+
+    std.os.getrandom(std.mem.asBytes(&seed)) catch |err| {
+        stdout.print("Unable to seed random, defaulting to 0\n", .{}) catch unreachable;
+        seed = 0;
+    };
+    
+    var rand = std.rand.DefaultPrng.init(seed);
+    const picked_num : i32 = rand.random.intRangeAtMost(u8, lowest, highest);
     var buffer: [100]u8 = undefined;
-    var run_loop = true;
     
-    while ( run_loop ) {
+    // Start off by prompting the user
+    // catch unreachable means this shouldn't error, in safe mode this will trigger a panic/crash
+    // in unsafe mode the behavior is undefined
+    stdout.print(
+        "Guess a number between {} and {} (inclusive) or 'q' to quit.\n", 
+        .{lowest, highest}) catch unreachable;
 
-        writer.print("Your guess is: ", .{}) catch return false;
+    while ( true ) {
+
+        stdout.print("Your guess is: ", .{}) catch unreachable;
         
-        const input = (try nextLine(reader, &buffer)).?;
+        // We fetch the input from stdin
+
+        // what is this mess? It's easy! 
+        var input = (nextLine(stdin, &buffer) catch {
+            stdout.print("Error reading input, exiting\n", .{}) catch return;
+            return;
+            }).?;
+
+        // Compare our input to "q" or "Q", if it is, quit
         if ( std.ascii.eqlIgnoreCase(input,"q")) {
-            writer.print("Later scumbag\n", .{}) catch return false;
-            return false;
+            stdout.print("Later scumbag\n", .{}) catch return;
+            return;
         }
-    
-        var input_val = std.fmt.parseInt(i32, input, 10) catch |err| {
-            writer.print("Invalid value\n", .{}) catch return false;
+   
+        // Convert our ascii input to an int
+        // Zig doesn't have exceptions instead a function can be set to
+        // return either a val or an error set, it does this by making
+        // a union type of the possible return value and the error set.
+
+        // The following spilts the return/error set into the actual return
+        // value and catching the error. If the catch |err| was not there
+        // input_val would fail as you can store a i32,error set union as a
+        // i32 type.
+        var input_val : i32 = std.fmt.parseInt(i32, input, 10) catch {
+            stdout.print("Invalid value\n", .{}) catch {};
             continue;
         };
 
-        if ( input_val > picked_num ) {
-            writer.print("Your pick is too large.\n", .{}) catch {};
+        if ( input_val > highest or input_val < lowest ) {
+            stdout.print("Pick out of bounds, choose a number between {} and {}.\n", 
+            .{lowest, highest}) catch {};
+        } else if ( input_val > picked_num ) {
+            stdout.print("Your pick is too large.\n", .{}) catch {};
         } else if ( input_val < picked_num ) {
-            writer.print("Your pick is too small\n", .{}) catch return false;
+            stdout.print("Your pick is too small\n", .{}) catch {};
         } else {
-            writer.print("Winner winner, chicken dinner!\n", .{}) catch return false;
-            return true;
+            stdout.print("Winner winner, chicken dinner!\n", .{}) catch {};
+            return;
         }
-
     }
-   
-    return true;
-}
-
-pub fn main() !void {
-    const stdin = std.io.getStdIn();
-    const stdout = std.io.getStdOut();
-
-    stdout.writeAll(
-        "Guess a number between 1 and 100 (inclusive) or 'q' to quit.\n",
-    ) catch unreachable;
-
-    var seed: u64 = undefined;
-    try std.os.getrandom(std.mem.asBytes(&seed));
-    var rand = std.rand.DefaultPrng.init(seed);
-    
-    const picked_num : i32 = rand.random.intRangeAtMost(u8, lowest, highest);
-
-    _ = gameLoop(stdin.reader(), stdout.writer(), picked_num) catch unreachable;
-        
+    return;
 }
 
