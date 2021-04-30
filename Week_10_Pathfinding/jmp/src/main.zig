1b12ed6a650d8d3fcaba26e1433412e707d63455
jim price <shadeops@gmail.com>
2021-04-29 23:22:24 -0700

    Dijkstra with a few wrong turns

--- Week_10_Pathfinding/jmp/src/main.zig
+++ Week_10_Pathfinding/jmp/src/main.zig
@@ -4,8 +4,8 @@ const ray = @cImport({
     @cInclude("raylib.h");
 });
 
-const tile_size = 16;
-const tile_pad = 2;
+const tile_size = 8;
+const tile_pad = 1;
 
 fn makeMap(comptime T: type, allocator: *std.mem.Allocator, file_path: []const u8) !std.ArrayList(T) {
     const cwd = std.fs.cwd();
@@ -74,9 +74,12 @@ fn drawBoard(board: std.ArrayList(u1), path: std.ArrayList(u1)) !void {
         } else {
             ray.DrawRectangleRec(indexToBox(board_size, i), ray.DARKGRAY);
         }
-        
+
         if (path.items[i] == 1) {
-            ray.DrawRectangleRec(indexToBox(board_size, i), ray.Color{.r = 255, .g=255, .b=0, .a=128});
+            ray.DrawRectangleRec(
+                indexToBox(board_size, i),
+                ray.Color{ .r = 255, .g = 255, .b = 0, .a = 128 },
+            );
         }
     }
 }
@@ -113,8 +116,13 @@ fn neighbourIterator(tile: usize, grid_size: usize) NeighbourIterator {
     };
 }
 
-fn makeBFSMap(allocator: *std.mem.Allocator, start: usize, board: std.ArrayList(u1)) !std.ArrayList(usize) {
-    var fifo = std.fifo.LinearFifo(usize, .{ .Static = 32 }).init();
+fn makeBFSMap(
+    allocator: *std.mem.Allocator,
+    start: usize,
+    end: ?usize,
+    board: std.ArrayList(u1),
+) !std.ArrayList(usize) {
+    var fifo = std.fifo.LinearFifo(usize, .Dynamic).init(allocator);
     defer fifo.deinit();
 
     try fifo.writeItem(start);
@@ -129,6 +137,9 @@ fn makeBFSMap(allocator: *std.mem.Allocator, start: usize, board: std.ArrayList(
 
     while (fifo.readableLength() != 0) {
         var current = fifo.readItem().?;
+
+        if (end != null and current == end.?) break;
+
         var iter = neighbourIterator(current, map_length);
         while (iter.next()) |next| {
             if (board.items[next] == 0) {
@@ -144,7 +155,79 @@ fn makeBFSMap(allocator: *std.mem.Allocator, start: usize, board: std.ArrayList(
     return map;
 }
 
-fn makeBFSPath(allocator: *std.mem.Allocator, start: usize, end: usize, bfs: std.ArrayList(usize)) !std.ArrayList(u1) {
+fn lessThan(a: IndexPriority, b: IndexPriority) std.math.Order {
+    return std.math.order(a.priority, b.priority);
+}
+
+fn greaterThan(a: IndexPriority, b: IndexPriority) std.math.Order {
+    return lessThan(a, b).invert();
+}
+
+const IndexPriority = struct {
+    idx: usize,
+    priority: u32,
+};
+
+fn makeDijkstraMap(
+    allocator: *std.mem.Allocator,
+    start: usize,
+    end: ?usize,
+    board: std.ArrayList(u1),
+    costs: std.ArrayList(u32),
+) !std.ArrayList(usize) {
+    var map_length = @intCast(usize, board.items.len);
+    var unchecked = map_length;
+
+    var map = std.ArrayList(usize).init(allocator);
+    try map.resize(map_length);
+
+    for (map.items) |*v| v.* = unchecked;
+
+    var max_cost: u32 = 0;
+    for (costs.items) |*v| max_cost = std.math.max(max_cost, v.*);
+
+    var current_costs = std.ArrayList(u32).init(allocator);
+    defer current_costs.deinit();
+    try current_costs.resize(map_length);
+
+    for (current_costs.items) |*v| v.* = 0;
+
+    var queue = std.PriorityQueue(IndexPriority).init(allocator, lessThan);
+    defer queue.deinit();
+    try queue.ensureCapacity(map_length);
+
+    try queue.add(.{ .idx = start, .priority = 0 });
+    current_costs.items[start] = max_cost - costs.items[start];
+
+    while (queue.removeOrNull()) |current_priority| {
+        var current = current_priority.idx;
+        if (end != null and current == end.?) break;
+
+        var iter = neighbourIterator(current, map_length);
+        while (iter.next()) |next| {
+            // Skip non valid travel tiles
+            if (board.items[next] == 0) {
+                continue;
+            }
+            var new_cost = current_costs.items[current] + (max_cost - costs.items[next]);
+
+            if (map.items[next] == unchecked or new_cost < current_costs.items[next]) {
+                current_costs.items[next] = new_cost;
+                try queue.add(.{ .idx = next, .priority = new_cost });
+                map.items[next] = current;
+            }
+        }
+    }
+    std.debug.print("Cost of end: {}\n", .{current_costs.items[map_length - 1]});
+    return map;
+}
+
+fn makeBFSPath(
+    allocator: *std.mem.Allocator,
+    start: usize,
+    end: usize,
+    bfs: std.ArrayList(usize),
+) !std.ArrayList(u1) {
     var map = std.ArrayList(u1).init(allocator);
     try map.resize(bfs.items.len);
     for (map.items) |*v| v.* = 0;
@@ -168,19 +251,28 @@ pub fn main() anyerror!void {
     var act1 = try makeMap(u1, &arena.allocator, "../data/act1.csv");
     defer act1.deinit();
     std.debug.print("Cells: {}\n", .{act1.items.len});
-    // var act2 = try makeMap(u1, &arena.allocator, "../data/act2.csv");
-    // defer act2.deinit();
-    // std.debug.print("Cells: {}\n", .{act2.items.len});
+    var act2 = try makeMap(u1, &arena.allocator, "../data/act2.csv");
+    defer act2.deinit();
+    var act2_speeds = try makeMap(u32, &arena.allocator, "../data/act2_speeds.csv");
+    defer act2_speeds.deinit();
+    std.debug.print("Cells: {}\n", .{act2.items.len});
     // var act3 = try makeMap(u1, &arena.allocator, "../data/act3_water.csv");
     // defer act3.deinit();
     // std.debug.print("Cells: {}\n", .{act3.items.len});
 
-    var board_size = @intCast(i32, std.math.sqrt(act1.items.len));
+    var act = act2;
+    var start: usize = 0;
+    var end = act.items.len - 1;
 
-    var act1_bfs_map = try makeBFSMap(&arena.allocator, 0, act1);
-    defer act1_bfs_map.deinit();
+    var board_size = @intCast(i32, std.math.sqrt(act.items.len));
 
-    var path_map = try makeBFSPath(&arena.allocator, 0, 399, act1_bfs_map);
+    var bfs_map = try makeBFSMap(&arena.allocator, start, null, act);
+    defer bfs_map.deinit();
+
+    var ucs_map = try makeDijkstraMap(&arena.allocator, start, null, act2, act2_speeds);
+    defer ucs_map.deinit();
+
+    var path_map = try makeBFSPath(&arena.allocator, start, end, ucs_map);
     defer path_map.deinit();
 
     ray.InitWindow(board_size * tile_size, board_size * tile_size, "Zigzag");
@@ -193,9 +285,43 @@ pub fn main() anyerror!void {
         ray.BeginDrawing();
         ray.ClearBackground(ray.BLACK);
 
-        try drawBoard(act1, path_map);
+        try drawBoard(act, path_map);
 
         ray.DrawFPS(50, 50);
         ray.EndDrawing();
     }
+}
+
+test "simple compare" {
+    var a: usize = 1;
+    var b: usize = 1;
+    std.testing.expect(a == b);
+}
+
+test "null compare" {
+    const a = null;
+    std.testing.expect(a == null);
+}
+
+test "optional compare with null" {
+    var a: ?usize = null;
+    std.testing.expect(a == null);
+}
+
+test "optional compare with value" {
+    var a: ?usize = 1;
+    std.testing.expect(a != null);
+}
+
+// https://github.com/ziglang/zig/issues/6059
+// test "optional compare with value and compare" {
+//     var a: ?usize = 1;
+//     var b: usize = 1;
+//     std.testing.expect(a != null and a == b);
+// }
+
+test "optional compare with value and proper compare" {
+    var a: ?usize = null;
+    var b: usize = 1;
+    std.testing.expect(a != null and a.? == b);
 }
